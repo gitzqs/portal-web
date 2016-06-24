30dc0db03d85f65cdb3b5f3cb818720be7c7d3a6
zqs <823969832@qq.com>
2016-06-24 17:21:13 +0800

    update

---
+++ portal-web/src/main/webapp/document/init.sql
@@ -0,0 +1,10 @@
+
+--import data for sys_member -- 
+INSERT INTO `sys_member` VALUES ('1', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '15205155720');
+
+-- import data for sys_url --
+INSERT INTO `sys_url` VALUES ('1', '主页设置', null, '1', null);
+INSERT INTO `sys_url` VALUES ('2', '个人简介', '/home/personal_profile', '2', '1');
+INSERT INTO `sys_url` VALUES ('3', '工作经历', '/home/work_experience', '2', '1');
+INSERT INTO `sys_url` VALUES ('4', '项目案例', '/home/project_ex', '2', '1');
+INSERT INTO `sys_url` VALUES ('5', '未完待续', '/home/to_be_continue', '2', '1');
